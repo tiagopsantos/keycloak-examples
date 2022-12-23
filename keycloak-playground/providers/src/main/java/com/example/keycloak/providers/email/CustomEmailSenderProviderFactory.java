c109ef84114c413718b14b102c20030200c8ea2b
tiagopsantos <tiago.pereira.santos@gmail.com>
2022-12-23 20:09:39 +0000

    Custom

--- keycloak-playground/providers/src/main/java/com/example/keycloak/providers/email/CustomEmailSenderProviderFactory.java
+++ keycloak-playground/providers/src/main/java/com/example/keycloak/providers/email/CustomEmailSenderProviderFactory.java
@@ -1,36 +1,35 @@
 package com.example.keycloak.providers.email;
 
-import lombok.extern.jbosslog.JBossLog;
-import org.keycloak.Config.Scope;
-import org.keycloak.email.EmailSenderProvider;
-import org.keycloak.email.EmailSenderProviderFactory;
+import org.keycloak.Config;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
 
-@JBossLog
-public class CustomEmailSenderProviderFactory implements EmailSenderProviderFactory {
+import org.keycloak.email.*;
+/**
+ * @author <a href="mailto:tiago.santos@link.pt">Tiago Santos</a>
+ * Copia - https://github.com/keycloak/keycloak/blob/main/services/src/main/java/org/keycloak/email/DefaultEmailSenderProvider.java
+ */
+public class CustomEmailSenderProviderFactory implements EmailSenderProviderFactory  {
+ 
+    @Override
+    public CustomEmailSenderProvider create(KeycloakSession session) {
+        return new CustomEmailSenderProvider(session);
+    }
 
-  @Override
-  public EmailSenderProvider create(KeycloakSession session) {
-    return new CustomEmailSenderProvider(session);
-  }
+    @Override
+    public void init(Config.Scope config) {
+    }
 
-  @Override
-  public void init(Scope scope) {
-  }
+    @Override
+    public void postInit(KeycloakSessionFactory factory) {
+    }
 
-  @Override
-  public void postInit(KeycloakSessionFactory sessionFactory) {
-  }
+    @Override
+    public void close() {
+    }
 
-  @Override
-  public void close() {
-  }
-
-  @Override
-  public String getId() {
-    return "default";
-    // a value different than default doesn't work
-//    return "custom";
-  }
+    @Override
+    public String getId() {
+        return "default";
+    }
 }
